7b88a758fc8129f19be6d5b667b4090ff64ceedd
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-01-04 22:15:46 +0100

    all the package installed and layout done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+
+function App() {
+  return (
+    <div className="app">
+      <div className='navbar' >
+        <Navbar />
+      </div>
+      <div className='main'>
+
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
