54c1f90a5150522794fa48894485bd34e1f2e01c
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-01-16 15:26:14 +0100

    layout done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Homepage, Exchanges, CryptoDetails, Cryptocurrencies, News } from './components';
 
 function App() {
   return (
@@ -11,11 +11,31 @@ function App() {
       <div className='navbar' >
         <Navbar />
       </div>
+
       <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
 
-      </div>
-      <div className='footer'>
-
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
