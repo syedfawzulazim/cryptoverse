ae755e90df02c29f5f67f398e277e64204357d02
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-01-17 23:15:13 +0100

    cryptoNews done

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -20,12 +20,15 @@ const Cryptocurrencies = ({ simplified }) => {
   if (isFetching) return "Loading...";
   return (
     <div>
-      <div className="search-crypto">
-        <Input
-          placeholder="Search Cryptocurrency"
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import { Typography, Select, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
-  return <div>News</div>;
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewscategoty] = useState("Cryptocurrency");
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 12,
+  });
+
+  const { data } = useGetCryptosQuery(100);
+
+  if (!cryptoNews?.value) return "Loading...";
+  return (
+    <div>
+      <Row gutter={[24, 24]}>
+        {!simplified && (
+          <Col span={24}>
+            <Select
+              showSearch
+              className="select-news"
+              placeholder="select a crypto"
+              optionLabelProp="children"
+              onChange={(value) => setNewscategoty(value)}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              <Option value="Cryptocurrency">Cryptocurrencies</Option>
+              {data?.data?.coins.map((coin) => (
+                <Option value={coin.name}>{coin.name}</Option>
+              ))}
+            </Select>
+          </Col>
+        )}
+        {cryptoNews.value.map((news, index) => (
+          <Col sx={24} sm={12} lg={8} key={index}>
+            <Card hoverable className="news-card">
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Title className="news-title" level={4}>
+                    {news.name}
+                  </Title>
+                  <img
+                    style={{ maxWidth: "200px", maxHeight: "100px" }}
+                    src={news?.image?.thumbnail?.contentUrl}
+                    alt="crypto-news"
+                  />
+                </div>
+                <p>{`${news.description.substring(0, 200)}...`}</p>
+                <div className="provider-container">
+                  <div>
+                    <Avatar
+                      src={news.provider[0]?.image?.thumbnail?.contentUrl}
+                    />
+                    <Text className="provider-name">
+                      {news.provider[0]?.name}
+                    </Text>
+                  </div>
+                  <Text>
+                    {moment(news.datePublished).startOf("ss").fromNow()}
+                  </Text>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default News;
