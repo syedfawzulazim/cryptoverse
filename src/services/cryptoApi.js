cc18f5899579ef6d2dc7746dfca65d396ba6b7f5
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-01-17 21:57:42 +0100

    redux and api services

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react/';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'ca5b1adcb0msh8cbc17af25fa452p159e3bjsn5c14472b7ae9'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
