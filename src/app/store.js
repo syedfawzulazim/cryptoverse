a145603deab57f8a3a4de0b04656f5b37a2783fe
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-03-08 02:38:36 +0100

    middleware added for APIs

--- src/app/store.js
+++ src/app/store.js
@@ -7,4 +7,6 @@ export default configureStore({
         [cryptoApi.reducerPath]: cryptoApi.reducer,
         [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat([cryptoApi.middleware, cryptoNewsApi.middleware]),
 });
